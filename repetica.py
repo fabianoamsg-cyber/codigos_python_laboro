0dc0aaa0c9311dda20efa0b1c04da78ac7487ca5
Fabiano Martins <fabianoamsg@gmail.com>
2025-09-17 20:17:31 -0300

    Trabalhando com for em python

---
+++ repetica.py
@@ -0,0 +1,25 @@
+"""
+for (etapa1; etapa2; etapa3){
+
+}
+"""
+#range() não conta o valo final, ele ignora. Se eu quiser exibir o valor final devo colocar ele + 1
+
+#EXEMPLO 1
+for contador in range (1,11):
+    print (contador,end=" ")
+
+
+print("=" * 120)
+
+#EXEMPLO 2
+#O -1 indica que o intervalo irá de -1 em -1, isto é o passo a passo do valor inicial até o valor final.
+for contador in range (10,0,-1):
+    print (contador,end=" ")
+
+    print("\n")
+    print("=" * 20)
+
+#EXEMPLO 3
+for contador in range(0, 21,2):
+     print (contador,end=", ")
